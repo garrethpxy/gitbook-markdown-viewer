bfea904cd8961f08d03b5b9dc707aaa6a470fb7d
Garreth Peh <garrethpeh@gmail.com>
2020-11-02 14:14:44 +0800

    minify images. ~40% file size reduction

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const MergeIntoSingle = require('./custom-webpack-merge-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 
@@ -15,6 +16,14 @@ const md = require('markdown-it')({
 
 module.exports = {
   publicPath: CONFIG.APP_BASE_URL,
+  chainWebpack: config => {
+    config.module
+      .rule('img')
+      .test(/\.(jpe?g|png|gif|svg)$/i)
+      .use('file-loader')
+        .loader('file-loader')
+        .end()
+  },
   configureWebpack: {
     plugins: [
       new MergeIntoSingle({
@@ -64,11 +73,11 @@ module.exports = {
            * Copy all gitbook's images into target folder
            */
           {
-            /*eslint-disable */
-            from: `\.gitbook/assets/**/*.png`,
+            from: `**/*.png`,
             /*eslint-enable */
-            context: `node_modules/${CONFIG.NODE_PACKAGE_NAME}`,
-            to: CONFIG.TARGET_DOC_FOLDER
+            context: `node_modules/${CONFIG.NODE_PACKAGE_NAME}/.gitbook/assets`,
+            /*eslint-disable */
+            to: CONFIG.TARGET_DOC_FOLDER + '/_assets'
           },
           /**
            * Transform Markdown into HTML and copy into target folder
@@ -82,15 +91,41 @@ module.exports = {
               // transform Markdown into html
               let parsed = md.render(content + "")
               // point all images to target folder
-              parsed =  parsed.replace(/img src=".*\.gitbook/g, `img src="${CONFIG.APP_BASE_URL}${CONFIG.TARGET_DOC_FOLDER}/.gitbook`)
+              parsed =  parsed.replace(/img src=".*\.gitbook\/assets/g, `img src="${CONFIG.APP_BASE_URL}${CONFIG.TARGET_DOC_FOLDER}/_assets`)
               // add github style anchor icon
               let linkIconHtml = `<svg aria-hidden="true" height="16" version="1.1" viewBox="0 0 16 16" width="16"><path d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path></svg>`;
               parsed =  parsed.replace(/(<a class="header-anchor".*">).*<\/a>/g, `$1${linkIconHtml}</a>`)
               return parsed;
             },
          },
+         {
+          from: `_redirects`,
+          context: `src`,
+          to: './'
+        },
         ]
-      })
+      }),
+      new ImageMinimizerPlugin({
+        minimizerOptions: {
+          // Lossless optimization with custom option
+          // Feel free to experiment with options for better result for you
+          plugins: [
+            ['gifsicle', { interlaced: true }],
+            ['jpegtran', { progressive: true }],
+            ['optipng', { optimizationLevel: 5 }],
+            [
+              'svgo',
+              {
+                plugins: [
+                  {
+                    removeViewBox: false,
+                  },
+                ],
+              },
+            ],
+          ],
+        },
+      }),
     ]
   }
 }
